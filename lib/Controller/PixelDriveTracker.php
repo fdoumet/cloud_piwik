dfa3208c123e41b6e22a8a7597d5dcb96d8e72ac
Francis <fdoumet@gmail.com>
2018-09-21 17:17:48 -0700

    TODO

--- lib/Controller/PixelDriveTracker.php
+++ lib/Controller/PixelDriveTracker.php
@@ -14,6 +14,7 @@ use PiwikTracker;
 
 class PixelDriveTracker extends PiwikTracker{
 	public function __construct(){
+		//TODO: Get this from the app directly
 		$analyticsUrl = getenv('MLVX_ANALYTICS_URL');
 		parent::__construct(1, $analyticsUrl);
 	}
